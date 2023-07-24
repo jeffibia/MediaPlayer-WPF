690b7397835d25234f9cb9e86fc59b52c709d5ff
Fernando Castro <castro.fernando@me.com>
2023-07-23 23:16:08 -0300

    Versao 1.0

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using MediaPlayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ViewModel.PlaylistViewModel vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new ViewModel.PlaylistViewModel();
+            DataContext = vm;
+            //Play
+            vm.PlayRequested += (e) =>
+            {
+                if(this.MediaPlayerElement.Source != new Uri(e.Path.ToString()))
+                {
+                    this.MediaPlayerElement.Source = new Uri(e.Path.ToString());
+                }
+                this.MediaPlayerElement.Play();
+            };
+            //Stop
+            vm.StopRequested += (e) =>
+            {
+                this.MediaPlayerElement.Source = null;
+                this.MediaPlayerElement.Stop();
+                vm.SelectedMedia.Duration = "";
+            };
+            //Pause
+            vm.PauseRequested += (e) =>
+            {
+                this.MediaPlayerElement.Pause();
+            };
+            //Media finished play
+            this.MediaPlayerElement.MediaEnded += MediaPlayerElement_MediaEnded;
+            //Media opened
+            this.MediaPlayerElement.MediaOpened += MediaPlayerElement_MediaOpened;
+        }
+
+        private void MediaPlayerElement_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            var duration = this.MediaPlayerElement.NaturalDuration.TimeSpan.ToString();
+            vm.SelectedMedia.Duration = "Tempo total: " + duration;
+        }
+
+        //detect that movie/audio finished, so go to next one
+        private void MediaPlayerElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            vm.NextMediaCommand();
+        }
+    }
+}
---
+++ Model/MediaItem.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayer.Model
+{
+    public class MediaItem : ObservableObject, ICloneable
+    {
+        private Guid _id;
+        private string _path;
+        private string _title;
+        private string _duration;
+        public Guid Id
+        {
+            get => _id;
+            set => SetProperty(ref _id, value);
+        }
+        public string Path
+        {
+            get => _path;
+            set => SetProperty(ref _path, value);
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public string Duration
+        {
+            get => _duration;
+            set => SetProperty(ref _duration, value);
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
